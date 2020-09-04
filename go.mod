69f4c144fa26daf6227d71ec090b7150d4b49b8b
winterssy <mysu1214@gmail.com>
2020-09-04 12:26:19 +0800

    improvement: update

--- go.mod
+++ go.mod
@@ -7,16 +7,17 @@ require (
 	github.com/gin-gonic/gin v1.5.0
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/golang/protobuf v1.3.3 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stretchr/testify v1.4.0
-	github.com/winterssy/ghttp v0.0.0-20200310005252-b14cb4acc8a2
+	github.com/winterssy/ghttp v0.0.0-20200904031839-b68c76d629d9
 	github.com/winterssy/gjson v0.0.0-20200306020332-1f68efaec187
 	github.com/winterssy/glog v0.0.0-20200305052031-8e145d5ae4ef
-	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
-	golang.org/x/sys v0.0.0-20200212091648-12a6c2dcc1e4 // indirect
-	golang.org/x/text v0.3.2
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	golang.org/x/text v0.3.3
+	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
 	gopkg.in/go-playground/validator.v9 v9.31.0 // indirect
 )
--- pkg/cryptography/cryptography.go
+++ pkg/cryptography/cryptography.go
@@ -10,7 +10,7 @@ import (
 
 func AESCBCEncrypt(plainText, key, iv []byte) []byte {
 	block, _ := aes.NewCipher(key)
-	plainText = pkcs5Padding(plainText, block.BlockSize())
+	plainText = pkcs7Padding(plainText, block.BlockSize())
 	blockMode := cipher.NewCBCEncrypter(block, iv)
 	cipherText := make([]byte, len(plainText))
 	blockMode.CryptBlocks(cipherText, plainText)
@@ -22,13 +22,13 @@ func AESCBCDecrypt(cipherText, key, iv []byte) []byte {
 	blockMode := cipher.NewCBCDecrypter(block, iv)
 	plainText := make([]byte, len(cipherText))
 	blockMode.CryptBlocks(plainText, cipherText)
-	plainText = pkcs5UnPadding(plainText)
+	plainText = pkcs7UnPadding(plainText)
 	return plainText
 }
 
 func AESECBEncrypt(plainText, key []byte) []byte {
 	block, _ := aes.NewCipher(key)
-	plainText = pkcs5Padding(plainText, block.BlockSize())
+	plainText = pkcs7Padding(plainText, block.BlockSize())
 	blockMode := NewECBEncrypter(block)
 	cipherText := make([]byte, len(plainText))
 	blockMode.CryptBlocks(cipherText, plainText)
@@ -40,17 +40,17 @@ func AESECBDecrypt(cipherText, key []byte) []byte {
 	blockMode := NewECBDecrypter(block)
 	plainText := make([]byte, len(cipherText))
 	blockMode.CryptBlocks(plainText, cipherText)
-	plainText = pkcs5UnPadding(plainText)
+	plainText = pkcs7UnPadding(plainText)
 	return plainText
 }
 
-func pkcs5Padding(src []byte, blockSize int) []byte {
+func pkcs7Padding(src []byte, blockSize int) []byte {
 	padding := blockSize - len(src)%blockSize
 	paddingText := bytes.Repeat([]byte{byte(padding)}, padding)
 	return append(src, paddingText...)
 }
 
-func pkcs5UnPadding(src []byte) []byte {
+func pkcs7UnPadding(src []byte) []byte {
 	n := len(src)
 	unPadding := int(src[n-1])
 	return src[:n-unPadding]
